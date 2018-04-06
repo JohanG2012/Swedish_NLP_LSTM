f0dcdb7f60c3e731d02474af5d1a37f6e341dba6
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-04-07 00:03:25 +0200

    Main module

---
+++ run_network.py
@@ -0,0 +1,12 @@
+from download_data import download_files
+from create_training_sets import create_trainingsets
+from use_lstm_model import use_lstm_model
+from train_lstm_model import train_lstm_model
+
+if __name__ == "__main__":
+    #download_files()
+    #parse_xml()
+    #preprocess_sentences()
+    #create_trainingsets()
+    #train_lstm_model()
+    #use_lstm_model()
