3e176284470de62243099024eb5834e326c16199
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-05-13 05:49:36 +0200

    Fix typos

--- run_network.py
+++ run_network.py
@@ -1,14 +1,14 @@
 from download_data import download_files
 from create_training_sets import create_trainingsets
-from use_lstm_model import use_lstm_model
-from train_lstm_model import train_lstm_model
-from preporcess_data import parse_xml
-from preporcess_data import preprocess_sentences
+#from use_lstm_model import use_lstm_model
+#from train_lstm_model import train_lstm_model
+from preprocess_data import parse_xml
+from preprocess_data import preprocess_sentences
 
 if __name__ == "__main__":
     #download_files()
     #parse_xml()
     #preprocess_sentences()
-    #create_trainingsets()
+    create_trainingsets()
     #train_lstm_model()
     #use_lstm_model()
