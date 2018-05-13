3d9dd58a7a528adf04f6471e75bb87edc74f77e6
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-05-13 05:47:30 +0200

    Add write_appart and gender methods

--- noise_maker.py
+++ noise_maker.py
@@ -1,25 +1,30 @@
 import pickle
+import re
 import numpy as np
 
 DATA_FOLDER = './data'
 
+vocab_to_int = pickle.load(open("{}/vocab_to_int.pkl".format(DATA_FOLDER), "rb"))
+int_to_vocab = pickle.load(open("{}/int_to_vocab.pkl".format(DATA_FOLDER), "rb"))
+
+top_words = pickle.load(open("{}/top_words.pkl".format(DATA_FOLDER), "rb"))
+
+apart_pattern = re.compile(r'({})(\S+)'.format("|".join(top_words)), re.I)
+
 def noise_maker(sentence, threshold):
-    vocab_to_int = pickle.load(open("{}/vocab_to_int.pkl".format(DATA_FOLDER), "rb"))
+
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
            'n','o','p','q','r','s','t','u','v','w','x','y','z','å', 'ä', 'ö']
     noisy_sentence = []
-    jump = 0
     i = 0
     while i < len(sentence):
         random = np.random.uniform(0,1,1)
 
         # threshold high = Most characters correct
-        if random < threshold or jump == 1:
+        if random < threshold:
             noisy_sentence.append(sentence[i])
-            jump = 0
         else:
             new_random = np.random.uniform(0,1,1)
-            jump = 1
             # ~33% chance - swap locations
             if new_random > 0.67:
                 if i == (len(sentence) - 1):
@@ -40,3 +45,13 @@ def noise_maker(sentence, threshold):
                 pass
         i += 1
     return noisy_sentence
+
+def write_apart(sentence):
+    sentence = "".join([int_to_vocab[i] for i in sentence])
+    written_apart = re.sub(apart_pattern, r'\g<1> \g<2>', sentence)
+    return [vocab_to_int[letter] for letter in written_apart]
+
+def switch_gender(sentence):
+    sentence = "".join([int_to_vocab[i] for i in sentence])
+    gender_bender = " ".join(['en' if w == 'ett' else 'ett' if w == 'en' else w for w in sentence.split()])
+    return [vocab_to_int[letter] for letter in gender_bender]
